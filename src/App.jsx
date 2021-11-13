8eb7c838b11dc227ba829b281b3d9de48de06042
Ken S <gxcadr@gmail.com>
2021-11-13 00:37:43 -0800

    delete functionality working, state added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
 import React, {Component} from 'react';
+import Table from './Table';
 
 class App extends Component {
+  state = {
+    characters: [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspiring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ],
+  }
+
+  removeCharacter = (index) => {
+    const { characters } = this.state;
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      }),
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>Hello, React!</h1>
+      <div className="container">
+        <Table characterData={this.state.characters} removeCharacter={this.removeCharacter} />
       </div>
     )
   }
